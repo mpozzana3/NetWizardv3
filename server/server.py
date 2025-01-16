c708ed54b797b50ba54a9b26928b4e1366d92755
mpozzana3 <167103726+mpozzana3@users.noreply.github.com>
2025-01-16 15:55:10 +0100

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../server')))
+
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
